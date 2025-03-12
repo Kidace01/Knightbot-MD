69f540942434c8a4ad46e46a5aa5b265532475c3
Kidace01 <161967625+Kidace01@users.noreply.github.com>
2025-03-12 18:50:36 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'KIDACE', // Your name
+  ownerNumber: '27609940136', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
